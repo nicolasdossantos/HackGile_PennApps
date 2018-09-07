d15344d990838b0410704777eadb17b0f0ad822c
Nick DeFilippis <ndefilippis98@gmail.com>
2018-09-07 19:37:53 -0400

    Update createDatabase

--- createDatabase.sql
+++ createDatabase.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE pennapps;
 
-GRANT ALL PRIVILEGES ON pennapps TO 'webuser'@'localhost'
+GRANT ALL PRIVILEGES ON pennapps.* TO 'webuser'@'localhost'
 IDENTIFIED BY 'pennapps2018';
 
 USE pennapps;
