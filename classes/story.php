b3004204f181602a57308b59dc1967a68a3849bf
Thomas Chin <tchin25@gmail.com>
2018-09-08 04:03:08 -0400

    Story Class Update

--- classes/story.php
+++ classes/story.php
@@ -4,9 +4,11 @@ class story
 {
     public $substory = array();
     public $isComplete = false;
-    public $priority;
-    public $title;
-    public $description;
+    public $isSubstory = false;
+    public $parentStory;
+    protected $title;
+    protected $description;
+    protected $priority;
 
     public function __construct($args=[]) {
         $this->title = $args['title'] ?? '';
@@ -14,15 +16,46 @@ class story
         $this->priority = $args['priority'] ?? '';
     }
 
-    public function edit_story_title(){
-
+    public function edit_story_title($title){
+        $this->title = $title;
     }
 
-    public function edit_story_description(){
+    public function edit_story_description($description){
+        $this->description = $description;
+    }
 
+    public function complete(){
+        $this->isComplete = true;
+        if ($this->isSubstory){
+            $this->parentStory->check_substory_completion();
+        }
+    }
+
+    public function check_substory_completion(){
+        $temp = true;
+        foreach($this->substory as $story){
+            if($story->isComplete === false){
+                $temp = false;
+                break;
+            }
+        }
+        $this->isComplete = $temp;
+    }
+
+    public function add_substory($task){
+        $this->substory[] = $task;
+        $task->isSubstory = true;
+        $task->parentStory = $this;
     }
 
     public function delete_story(){
+        unset($this);
+    }
 
+    function __destruct()
+    {
+        foreach($this->substory as $story){
+            $story->delete_story();
+        }
     }
 }
