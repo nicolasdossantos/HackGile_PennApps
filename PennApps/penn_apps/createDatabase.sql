3f4df1e1df9cc001a6318eab3935ef72e67418c7
Nicolas dos Santos <nick@huntsman-ve701-0642.eduroam.wlan.private.upenn.edu>
2018-09-07 21:05:39 -0400

    Database data Updated

--- PennApps/penn_apps/createDatabase.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE pennapps;
-
-GRANT ALL PRIVILEGES ON pennapps.* TO 'webuser'@'localhost'
-IDENTIFIED BY 'pennapps2018';
-
-USE pennapps;
--- PennApps/penn_apps/private/db_credentials.php
+++ PennApps/penn_apps/private/db_credentials.php
@@ -8,6 +8,6 @@
 define("DB_SERVER", "localhost");
 define("DB_USER", "webuser");
 define("DB_PASS", "pennapps2018");
-define("DB_NAME", "chain_gang");
+define("DB_NAME", "pennapps");
 
 ?>
