2c64c0d930b7576cb8028c96b78a0bc7f0da91c7
Nick DeFilippis <ndefilippis98@gmail.com>
2018-09-08 18:52:41 -0400

    Add login and signup

--- HackGile/public/login.php
+++ HackGile/public/login.php
@@ -4,19 +4,35 @@
 
 <?php
 if(is_post_request()){
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $_SESSION['email'] = $email;
-    $_SESSION['fname'] = "Nick";
-    $_SESSION['lname'] = "DeFilippis";
-    $_SESSION['logged_in'] = true;
-    header("Location: member.php");
+    $email = mysqli_real_escape_string($database, $_POST['email']);
+    $hashed_password = hash("md5", mysqli_real_escape_string($database, $_POST['password']));
+
+    $sql = "SELECT * from members WHERE email = '$email' and hashed_password = '$hashed_password'";
+    $result =  member::find_by_sql($sql);
+
+    if (count($result) == 1){
+        $_SESSION['email'] = $email;
+        $_SESSION['fname'] = "Nick";
+        $_SESSION['lname'] = "DeFilippis";
+        $_SESSION['logged_in'] = true;
+        header("Location: member.php");
+    }
+    else{
+        $statusCode = 401;
+        $statusMessage = "Invalid Credentials";
+        header("Location: login.php");
+        header('Status: '.$statusCode.' '.$statusMessage);
+    }
+
 }
 ?>
 
 <div class="container white z-depth-2" style="padding-top: 10px; padding-bottom: 10px; margin-top: 100px;">
     <div class="row">
         <form method='POST' class="col s12" action="login.php">
+            <?php if(isset($statusCode)) { ?>
+                <h6 id="warning" style="padding: 20px;" class="red"><i class="material-icons left">cancel</i><?php echo $statusMessage?></h6>
+            <?php } ?>
             <h3>Login to HackGile</h3>
             <div class="input-field col s12">
                 <label for="email">Email</label>
@@ -29,3 +45,9 @@ if(is_post_request()){
         </form>
     </div>
 </div>
+
+<script>
+    $('document').ready(function(){
+        $('#warning').delay(2000).hide(250);
+    });
+</script>
