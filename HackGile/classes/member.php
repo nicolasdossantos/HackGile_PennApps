05798e2cf5b16f3bd9ea2519de2c1a1cbd335f74
angelawieber <tuf92449@temple.edu>
2018-09-08 06:12:20 -0400

    member and sprint classes

--- HackGile/classes/member.php
+++ HackGile/classes/member.php
@@ -5,4 +5,12 @@ class member
     public $name;
     public $email;
 
+    public function __construct($args = []) {
+        $this->name = $args['name'] ?? '';
+        $this->email = $args['email'] ?? '';
+    }
+
+    public function deleteMember() {
+        unset($this);
+    }
 }
