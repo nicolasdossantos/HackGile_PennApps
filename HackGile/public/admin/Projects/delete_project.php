99752746bcdb61fc695d8467551aef37b46047dc
Nicolas dos Santos <nicolas182@icloud.com>
2018-09-09 01:14:12 -0400

    Delete project

--- HackGile/public/admin/Projects/delete_project.php
+++ HackGile/public/admin/Projects/delete_project.php
@@ -4,22 +4,20 @@ require_once('../../../private/initialize.php');
 
 
 if(!isset($_GET['id'])) {
-    redirect_to(url_for('/staff/bicycles/index.php'));
+    redirect_to('../../project.php');
 }
 $id = $_GET['id'];
 $project = project::find_by_id($id);
 if($project == false) {
-    redirect_to(url_for('/staff/bicycles/index.php'));
+    redirect_to('../../project.php');
 }
 
 if(is_post_request()) {
 
-    // Delete bicycle
-    $result = $project->delete();
-    redirect_to(url_for('/staff/bicycles/index.php'));
 
-} else {
-    // Display form
+    $result = $project->delete();
+    redirect_to('../../project.php');
+
 }
 
 ?>
@@ -29,16 +27,16 @@ if(is_post_request()) {
 
 <div id="content">
 
-    <a class="back-link" href="<?php echo url_for('/staff/bicycles/index.php'); ?>">&laquo; Back to List</a>
+    <a class="back-link" href="<?php echo url_for('project.php'); ?>">&laquo; Backt</a>
 
     <div class="project delete">
         <h1>Delete Project</h1>
-        <p>Are you sure you want to delete this bicycle?</p>
+        <p>Are you sure you want to delete this project?</p>
         <p class="item"><?php echo h($project->name); ?></p>
 
         <form action="<?php echo url_for('/admin/Projects/delete_project.php?id=' . h(u($id))); ?>" method="post">
             <div id="operations">
-                <input type="submit" name="commit" value="Delete Bicycle" />
+                <input type="submit" name="commit" value="Delete Project" />
             </div>
         </form>
     </div>
